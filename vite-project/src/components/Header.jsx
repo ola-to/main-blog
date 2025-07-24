a9f8fbbf28aeaa0cc2f7733780b87eaf5eb5e873
tosin <reflextosin@gmail.com>
2025-07-24 10:41:16 +0100

    create and run the server

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
         className='self-center text-sm font-semibold whitespace-nowrap sm:text-xl dark:text-white'
       >
         <span className='px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-          Sahand's
+          Gold's
         </span>
         Blog
       </Link>
@@ -28,11 +28,11 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+        <Button className='hidden w-12 h-10 sm:inline' color='gray' pill>
             <FaMoon />
         </Button>
         <Link to='/sign-in' >
-            <Button gradientDuoTone='purpleToBlue' >
+            <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
         </Link>
