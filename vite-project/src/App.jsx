de464a396cdf89f69d9336187972bb03618b922c
tosin <reflextosin@gmail.com>
2025-07-24 10:11:12 +0100

    create the header section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,21 +5,23 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Project from './pages/Project'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home />} /> 
-      <Route path="/about"element={<About />} />
-      <Route path="/sign-in"element={<Signin />} />
-      <Route path="/sign-up"element={<Signup />} />
-      <Route path="/dashboard"element={<Dashboard />} />
-      <Route path="/project"element={<Project />} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/about"element={<About />} />
+        <Route path="/sign-in"element={<Signin />} />
+        <Route path="/sign-up"element={<Signup />} />
+        <Route path="/dashboard"element={<Dashboard />} />
+        <Route path="/project"element={<Project />} />
 
 
 
-     </Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
